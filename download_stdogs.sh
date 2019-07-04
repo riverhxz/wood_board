d7beff07c0d79291d997b9e69d3873b50cf61927
riverhxz <ravehun@qq.com>
2019-07-04 23:07:25 +0800

    fix augmenter

--- download_stdogs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-wget http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar
-wget http://vision.stanford.edu/aditya86/ImageNetDogs/annotation.tar
-tar xf images.tar
-tar xf annotation.tar
-mkdir data
-mv Images Annotation data
